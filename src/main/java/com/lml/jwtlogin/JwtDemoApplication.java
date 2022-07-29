c87ca3ffbd848a2eaffbaece746fd5469fb4fb41
MNilml <1613846577@qq.com>
2022-07-29 11:09:43 +0800

    测试

--- src/main/java/com/lml/jwtlogin/JwtDemoApplication.java
+++ src/main/java/com/lml/jwtlogin/JwtDemoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan(annotationClass = Repository.class, basePackages = "com.ylc.dao")
 public class JwtDemoApplication {
 
     public static void main(String[] args) {
